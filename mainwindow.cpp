2cf8fdb7e163ef73e7c664d959551e5e6b336f87
Folosman <63555425+Folosman@users.noreply.github.com>
2023-12-10 03:42:36 +0500

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,9 @@
 #include "ui_mainwindow.h"
 
 #include <QPushButton>
+#include <QListWidgetItem>
+
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,8 +19,10 @@ MainWindow::MainWindow(QWidget *parent)
 
     socket = new QBluetoothSocket(QBluetoothServiceInfo::RfcommProtocol);
 
-    connect(ui->m_findBtn, &QPushButton::clicked, this, &MainWindow::findClicked);
-
+    connect(ui->m_findBtn,      &QPushButton::clicked, this, &MainWindow::findClicked);
+    connect(ui->m_powerOffBtn,  &QPushButton::clicked, this, &MainWindow::powerOffClicked);
+    connect(ui->m_clearBtn,     &QPushButton::clicked, this, &MainWindow::clearBtnClicked);
+    connect(ui->m_blueList, SIGNAL(itemClicked(QListWidgetItem*)), this, SLOT(bluetoothListClicked(QListWidgetItem*)));
 }
 
 MainWindow::~MainWindow()
@@ -30,11 +35,25 @@ void MainWindow::findClicked()
     ui->m_blueList->clear();
     // bluetoothAgent->stop();
     bluetoothAgent->start();
+
+    discoveryAgent = new QBluetoothDeviceDiscoveryAgent(this);
+}
+
+void MainWindow::powerOffClicked()
+{
+    bluetoothAgent->stop();
+    discoveryAgentList = discoveryAgent->discoveredDevices();
+}
+
+void MainWindow::clearBtnClicked()
+{
+    ui->m_blueList->clear();
 }
 
 void MainWindow::discoveredDevice(const QBluetoothDeviceInfo &device)
 {
     ui->m_blueList->addItem(device.address().toString() + " " + device.name());
+    // socket->connectToService(device);
 }
 
 // void MainWindow::blueListClicked(QListWidgetItem *item)
@@ -42,3 +61,15 @@ void MainWindow::discoveredDevice(const QBluetoothDeviceInfo &device)
 //     contentString = item->text();
 //     ui->m_blueList->addItem(contentString);
 // }
+
+void MainWindow::bluetoothListClicked(QListWidget *item)
+{
+    // (discoveryAgentList.at(item->currentRow())).
+    socket->connectToService(QBluetoothAddress((discoveryAgentList.at(item->currentRow())).address()), QBluetoothUuid(QBluetoothUuid::SerialPort));
+    QByteArray recievedData = socket->readAll();
+    QFile file("log.txt");
+    file.open(QIODevice::WriteOnly);
+    file.write(recievedData);
+    file.close();
+
+}
